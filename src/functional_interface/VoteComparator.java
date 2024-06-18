d58ca50825b5b5a6c36ac1cc22db20aaf26e3aac
carlo8767 <carlo8767@gmail.com>
2024-06-18 12:40:20 +0200

    addMap

---
+++ src/functional_interface/VoteComparator.java
@@ -0,0 +1,54 @@
+package functional_interface;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class VoteComparator{
+
+
+
+
+
+    String name;
+    Integer vote;
+
+
+    public VoteComparator(String name, Integer vote){
+        this.name = name;
+        this.vote = vote;
+    }
+
+
+
+
+    public void sortByVote (List<VoteComparator> vcList){
+        vcList.sort((VoteComparator v1, VoteComparator v2)->
+                v1.getVote().compareTo(v2.getVote()));
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getVote() {
+        return vote;
+    }
+
+    public void setVote(Integer vote) {
+        this.vote = vote;
+    }
+
+
+
+
+
+}
+
+
+
