31e9779150b794f1493abf99c76e9fc1ff335357
carlo8767 <carlo8767@gmail.com>
2026-07-22 21:28:14 +0200

    agent_ollama

--- src/main/java/spring_hello/DemoApplication.java
+++ src/main/java/spring_hello/DemoApplication.java
@@ -3,11 +3,13 @@ package spring_hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class DemoApplication {
 
-    static void main(String[] args) {
+public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 }
--- src/main/java/spring_hello/configuration/AppConfiguration.java
+++ src/main/java/spring_hello/configuration/AppConfiguration.java
@@ -1,5 +1,8 @@
 package spring_hello.configuration;
 
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -7,12 +10,17 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
 import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 @EnableAspectJAutoProxy
 public class AppConfiguration {
+
+
+    @Value("${OLLAMA_API_KEY}")
+    private String name;
 
     @Bean
     public RestTemplate restTemplate() {
@@ -28,5 +36,22 @@ public class AppConfiguration {
         executor.setThreadNamePrefix("asyncExecution");
         executor.initialize();
         return executor;
+    }
+
+
+    @Bean
+    public ChatModel chatModelInit() {
+        String apyKey = "OLLAMA_API_KEY";
+
+        ChatModel chatModel = OllamaChatModel.builder()
+                .baseUrl("https://ollama.com")
+                .modelName("nemotron-3-nano:30b-cloud")
+                .customHeaders(Map.of(
+                        "Authorization", "Bearer " + apyKey
+                ))
+                .temperature(.3)
+                .numPredict(600)
+                .build();
+        return  chatModel;
     }
 }
